e549884195231a36a811066f8b758a876900a36c
nguyenthanhdat <87852737+ThanhDatwd@users.noreply.github.com>
2024-02-27 18:04:23 +0700

    23th-commit

--- src/connectors/axiosRestConnector.ts
+++ src/connectors/axiosRestConnector.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const restConnector = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_STATIC_API_URL,
+  baseURL: "https://tradingcme.com/v1/api",
 });
 
 export default restConnector;
